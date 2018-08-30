2628389bfd7cf4df1ca1f6a75a53b8018df29a88
唐李梅 <tanglimei@mininglamp.com>
2018-08-30 14:51:31 +0800

    modify

--- vue-small-project/solarSystem/src/App.vue
+++ vue-small-project/solarSystem/src/App.vue
@@ -1,17 +1,148 @@
 <template>
     <div id="app">
-        <router-view/>
+        <solar-system :ball-group="ballGroup" :center-sun = "centerSun" @clickBallCallback="clickCallback"></solar-system>
     </div>
 </template>
 <script>
+import solarSystem from '@/components/solarSystem';
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        solarSystem
+    },
+    data () {
+        return {
+            centerSun: {
+                centerSunImg: require('@/images/solar/sun.png'),
+                centerSunLabel: '数据应用服务',
+                centerSunWidth: '34em',
+                centerSunHeight: '37em',
+            },
+            ballGroup: [
+                {
+                    orbitR: '42em', // 轨道半径
+                    ballPositionDeg: 30, // 球初始位置角度
+                    ballLabel: '', // 球对应名称
+                    ballSize: (39 / 14) + 'em', // 球大小
+                    ballLabelColor: 'red', // 名称颜色
+                    speed: '10s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball21.png') // 球图片
+                },
+                {
+                    orbitR: '55em', // 轨道半径
+                    ballPositionDeg: 330, // 球初始位置角度
+                    ballLabel: 'ball1', // 球对应名称
+                    ballSize: (74 / 14) + 'em', // 球大小
+                    ballLabelColor: 'red', // 名称颜色
+                    speed: '50s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball31.png') // 球图片
+                },
+                {
+                    orbitR: '70em', // 轨道半径
+                    ballPositionDeg: 60, // 球初始位置角度
+                    ballLabel: 'ball2', // 球对应名称
+                    ballSize: 120 / 14 + 'em', // 球大小
+                    ballLabelColor: 'red', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball41.png') // 球图片
+                },
+                {
+                    orbitR: '70em', // 轨道半径
+                    ballPositionDeg: 150, // 球初始位置角度
+                    ballLabel: 'ball3', // 球对应名称
+                    ballSize: 134 / 14 + 'em', // 球大小
+                    ballLabelColor: '#fff', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball42.png') // 球图片
+                },
+                {
+                    orbitR: '90em', // 轨道半径
+                    ballPositionDeg: 210, // 球初始位置角度
+                    ballLabel: 'ball4', // 球对应名称
+                    ballSize: 178 / 14 + 'em', // 球大小
+                    ballLabelColor: 'orange', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball51.png') // 球图片
+                },
+                {
+                    orbitR: '90em', // 轨道半径
+                    ballPositionDeg: 100, // 球初始位置角度
+                    ballLabel: 'ball5', // 球对应名称
+                    ballSize: 104 / 14 + 'em', // 球大小
+                    ballLabelColor: '#55bef3', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball52.png') // 球图片
+                },
+                {
+                    orbitR: '90em', // 轨道半径
+                    ballPositionDeg: 270, // 球初始位置角度
+                    ballLabel: 'ball6', // 球对应名称
+                    ballSize: 82 / 14 + 'em', // 球大小
+                    ballLabelColor: '#eeb364', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball53.png') // 球图片
+                },
+                {
+                    orbitR: '90em', // 轨道半径
+                    ballPositionDeg: 0, // 球初始位置角度
+                    ballLabel: '', // 球对应名称
+                    ballSize: (35 / 14) + 'em', // 球大小
+                    ballLabelColor: 'red', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball54.png') // 球图片
+                },
+                {
+                    orbitR: '105em', // 轨道半径
+                    ballPositionDeg: 320, // 球初始位置角度
+                    ballLabel: 'ball7', // 球对应名称
+                    ballSize: 147 / 14 + 'em', // 球大小
+                    ballLabelColor: 'pink', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball61.png') // 球图片
+                },
+                {
+                    orbitR: '105em', // 轨道半径
+                    ballPositionDeg: 25, // 球初始位置角度
+                    ballLabel: 'ball8', // 球对应名称
+                    ballSize: 131 / 14 + 'em', // 球大小
+                    ballLabelColor: 'yellow', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball62.png') // 球图片
+                },
+                {
+                    orbitR: '105em', // 轨道半径
+                    ballPositionDeg: 180, // 球初始位置角度
+                    ballLabel: '', // 球对应名称
+                    ballSize: 84 / 14, // 球大小
+                    ballLabelColor: 'red', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball63.png') // 球图片
+                },
+                {
+                    orbitR: '105em', // 轨道半径
+                    ballPositionDeg: 240, // 球初始位置角度
+                    ballLabel: '', // 球对应名称
+                    ballSize: 14 / 14 + 'em', // 球大小
+                    ballLabelColor: 'red', // 名称颜色
+                    speed: '100s', // 每个轨道速度
+                    ballImg: require('@/images/solar/ball55.png') // 球图片
+                }
+            ]
+        }
+    },
+    methods: {
+        clickCallback ({item, index}) {
+            console.log(index);
+            console.log(item);
+        }
+    }
 }
 </script>
 <style lang="scss">
     html,body,#app{
         width:100%;
         height:100%;
+        overflow: hidden;
+        background:url('./images/index_bg.png') no-repeat center center;
     }
-//   @import "@/sass/reset.scss";
 </style>
--- vue-small-project/solarSystem/src/components/solarSystem.vue
+++ vue-small-project/solarSystem/src/components/solarSystem.vue
@@ -1,38 +1,31 @@
 <template>
-    <div class="solarBox opening">
-        <div id="universe" class="scale-stretched">
-            <div id="galaxy">
-                <div id="solar-system" class="earth">
-                    <div class="orbit"
-                        v-for="(item,index) in ballGroup"
-                        :key="index"
-                        :class="runIs"
-                        :style="{animationDuration:ballGroup[index].speed + 's',
-                        width:ballGroup[index].orbitR + 'em',height:ballGroup[index].orbitR + 'em',
-                        marginTop: '-' + (ballGroup[index].orbitR / 2) + 'em',
-                        marginLeft: '-' + (ballGroup[index].orbitR / 2) + 'em'}"
-                        @mouseover="hoverStop"
-                        @mouseout="outBall">
-                        <div class="pos"
-                        :class="runIs"
-                        :style="{ animationDuration:ballGroup[index].speed + 's',
-                        top: positionTop(ballGroup[index].orbitR, ballGroup[index].ballPositionDeg) + '%',
-                        left: positionLeft(ballGroup[index].orbitR, ballGroup[index].ballPositionDeg) + '%'}">
-                            <div class="planet"
-                            :style="{fontSize: ballGroup[index].ballSize + 'em',
-                            animationDuration:ballGroup[index].speed + 's',
-                            cursor: ballGroup[index].callback ? 'pointer':''}"
-                            @click="ballGroup[index].callback(item, index)">
-                            <img :src="ballGroup[index].ballImg" alt="">
-                            <p class="starLabel" :style="{color: ballGroup[index].ballLabelColor}">{{ballGroup[index].ballLabel}}</p>
-                            </div>
-                        </div>
-                    </div>
-                     <div id="sun">
-                         <img :src="centerSun.centerSunImg" alt="">
-                         <p class="sunLabel">{{centerSun.centerSunLabel}}</p>
+    <div class="galaxy">
+        <div class="solar-system">
+            <div class="orbit"
+                v-for="(item,index) in ballGroup"
+                :key="index"
+                :class="runIs"
+                :style="{animationDuration:ballGroup[index].speed,
+                width:ballGroup[index].orbitR,height:ballGroup[index].orbitR}"
+                @mouseover="hoverStop"
+                @mouseout="outBall">
+                <div class="pos"
+                :class="runIs"
+                :style="{ animationDuration:ballGroup[index].speed,
+                top: positionTop(parseInt(ballGroup[index].orbitR), parseInt(ballGroup[index].ballPositionDeg)) + '%',
+                left: positionLeft(parseInt(ballGroup[index].orbitR), parseInt(ballGroup[index].ballPositionDeg)) + '%'}">
+                    <div class="planet"
+                    :style="{fontSize: ballGroup[index].ballSize,
+                    animationDuration:ballGroup[index].speed}"
+                    @click="callbackBall(item, index)">
+                    <img :src="ballGroup[index].ballImg" alt="">
+                    <p class="starLabel" :style="{color: ballGroup[index].ballLabelColor}">{{ballGroup[index].ballLabel}}</p>
                     </div>
                 </div>
+            </div>
+                <div id="sun" :style="{width: centerSun.centerSunWidth,height: centerSun.centerSunHeight}">
+                    <img :src="centerSun.centerSunImg" alt="">
+                    <p class="sunLabel">{{centerSun.centerSunLabel}}</p>
             </div>
         </div>
     </div>
@@ -41,164 +34,15 @@
 export default {
     data () {
         return {
-            runIs: 'runningS',
-            centerSun: {
-                centerSunImg: require('../images/solar/sun.png'),
-                centerSunLabel: '数据应用服务',
-            },
-            // orbitR: [42, 60, 74, 74, 94, 94, 94, 94, 112, 112, 112, 112],
-            // ballPositionDeg: [30, 330, 60, 150, 210, 100, 270, 0, 320, 25, 180, 240],
-            // setLabel: ['', 'ball1', 'ball2', '', 'ball3', '', 'ball4', '', 'ball5', '', 'ball6'],
-            ballGroup: [
-                {
-                    orbitR: 42, // 轨道半径 单位为em，实际为42*14px
-                    ballPositionDeg: 30, // 球初始位置角度
-                    ballLabel: '', // 球对应名称
-                    ballSize: (39 / 14), // 球大小
-                    ballLabelColor: 'red', // 名称颜色
-                    speed: 10, // 每个轨道速度
-                    ballImg: require('../images/solar/ball21.png'), // 球图片
-                    callback: '' // 点击球的回调函数
-                },
-                {
-                    orbitR: 60, // 轨道半径
-                    ballPositionDeg: 330, // 球初始位置角度
-                    ballLabel: 'ball1', // 球对应名称
-                    ballSize: 74 / 14, // 球大小
-                    ballLabelColor: 'red', // 名称颜色
-                    speed: 50, // 每个轨道速度
-                    ballImg: require('@/images/solar/ball31.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 74, // 轨道半径
-                    ballPositionDeg: 60, // 球初始位置角度
-                    ballLabel: 'ball2', // 球对应名称
-                    ballSize: 120 / 14, // 球大小
-                    ballLabelColor: 'red', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball41.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 74, // 轨道半径
-                    ballPositionDeg: 150, // 球初始位置角度
-                    ballLabel: 'ball3', // 球对应名称
-                    ballSize: 134 / 14, // 球大小
-                    ballLabelColor: '#fff', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball42.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                        alert(123)
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 94, // 轨道半径
-                    ballPositionDeg: 210, // 球初始位置角度
-                    ballLabel: 'ball4', // 球对应名称
-                    ballSize: 178 / 14, // 球大小
-                    ballLabelColor: 'orange', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball51.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数名称
-                },
-                {
-                    orbitR: 94, // 轨道半径
-                    ballPositionDeg: 100, // 球初始位置角度
-                    ballLabel: 'ball5', // 球对应名称
-                    ballSize: 104 / 14, // 球大小
-                    ballLabelColor: '#55bef3', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball52.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 94, // 轨道半径
-                    ballPositionDeg: 270, // 球初始位置角度
-                    ballLabel: 'ball6', // 球对应名称
-                    ballSize: 82 / 14, // 球大小
-                    ballLabelColor: '#eeb364', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball53.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 94, // 轨道半径
-                    ballPositionDeg: 0, // 球初始位置角度
-                    ballLabel: '', // 球对应名称
-                    ballSize: 35 / 14, // 球大小
-                    ballLabelColor: 'red', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball54.png'), // 球图片
-                    callback: '' // 点击球的回调函数名称
-                },
-                {
-                    orbitR: 112, // 轨道半径
-                    ballPositionDeg: 320, // 球初始位置角度
-                    ballLabel: 'ball7', // 球对应名称
-                    ballSize: 147 / 14, // 球大小
-                    ballLabelColor: 'pink', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball61.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 112, // 轨道半径
-                    ballPositionDeg: 25, // 球初始位置角度
-                    ballLabel: 'ball8', // 球对应名称
-                    ballSize: 131 / 14, // 球大小
-                    ballLabelColor: 'yellow', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball62.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 112, // 轨道半径
-                    ballPositionDeg: 180, // 球初始位置角度
-                    ballLabel: '', // 球对应名称
-                    ballSize: 84 / 14, // 球大小
-                    ballLabelColor: 'red', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball63.png'), // 球图片
-                    callback: function (param, index) {
-                        console.log(param);
-                        console.log(index);
-                    } // 点击球的回调函数
-                },
-                {
-                    orbitR: 112, // 轨道半径
-                    ballPositionDeg: 240, // 球初始位置角度
-                    ballLabel: '', // 球对应名称
-                    ballSize: 14 / 14, // 球大小
-                    ballLabelColor: 'red', // 名称颜色
-                    speed: 100, // 每个轨道速度
-                    ballImg: require('../images/solar/ball55.png'), // 球图片
-                    callback: '' // 点击球的回调函数
-                }
-            ]
+            runIs: 'runningS'
+        }
+    },
+    props: {
+        ballGroup: {
+            type: Array
+        },
+        centerSun: {
+            type: Object
         }
     },
     methods: {
@@ -213,10 +57,14 @@ export default {
         },
         outBall () {
             this.runIs = 'runningS';
+        },
+        callbackBall (item, index) {
+            this.$emit('clickBallCallback', {item, index});
         }
     }
 };
 </script>
 <style lang="scss" scoped>
+    @import "@/sass/reset.scss";
     @import "@/sass/solar.scss";
 </style>
